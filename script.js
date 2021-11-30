fe6fb51ef7ec44d33a24d75a6a4376179c702e0d
Sofia Dersén <Sofia_dersen@hotmail.com>
2021-11-30 15:52:22 +0100

    Styling

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 window.addEventListener("scroll", function (e) {
-  if (window.scrollY < 400) {
-    document.body.style.backgroundColor = "blue";
+  if (window.scrollY < 600) {
+    document.body.style.backgroundColor = "var(--pink)";
   } else {
-    document.body.style.backgroundColor = "white";
+    document.body.style.backgroundColor = "var(--darker-pink)";
   }
 });
 
 // Potter section
+const checkOne = document.querySelector(".check-one");
+const btnContainer = document.querySelector(".btn-container");
+
+checkOne.addEventListener("click", () => {
+  potterHeads();
+});
 
 const potterImages = [
   "https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Hermione_Granger_poster.jpg/220px-Hermione_Granger_poster.jpg",
@@ -16,20 +22,30 @@ const potterImages = [
   "https://upload.wikimedia.org/wikipedia/en/1/10/RubeusHagrid.jpg",
 ];
 const characters = document.querySelector(".characters");
-const characterImage = document.createElement("img");
-const p = document.createElement("p");
-p.textContent = "Hej";
-p.textContent = "Hej";
 
-potterImages.forEach((potterImage) => {
-  const characterImage = document.createElement("img");
-  // characterImage.classList('character');
-  characterImage.src = potterImage;
-  console.log(potterImage);
+function potterHeads() {
+  potterImages.forEach((potterImage) => {
+    console.log(potterImage);
+    const img = document.createElement("img");
+    img.src = potterImage;
+    img.classList.add("character");
+    characters.appendChild(img);
+  });
+  const button = document.createElement("button");
+  button.textContent = "Ok go away...";
+  btnContainer.appendChild(button);
+
+  button.addEventListener("click", () => {
+    characters.classList.add("hidden");
+    button.classList.add("hidden");
+  });
+}
+
+//CONFETTI
+
+const checkTwo = document.querySelector(".check-two");
+const confetti = document.querySelector(".confetti-container");
+
+checkTwo.addEventListener("click", () => {
+  confetti.classList.toggle("hidden");
 });
-
-// const charachters = document.querySelector('.characters')
-// const checkOne = document.querySelector('.check-one');
-// checkOne.addEventListener('click', () => {
-//   charachters.classList.add('hidden');
-// })
